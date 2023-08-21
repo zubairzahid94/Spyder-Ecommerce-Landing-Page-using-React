37b3e3e506bca67767d15703f7b8b7433dae55df
Zubair <zubairzahid228@gmail.com>
2023-08-21 13:04:39 +0500

    fo-commit

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className='bg-cyan-600 w-full justify-center border-b-4 border-pink-500'>
     <div className='max-w-8xl w-full p-2' >
         <div className='flex items-center justify-between text-pink-00'>
-        <img src='/Images/Logo.png' className='h-10 fill-current'></img>
+        <img src='/Logo.png' className='h-10 fill-current'></img>
   
         <button className='text-gray-600 hover:text-gray-300'>
         <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/></svg>            </button>
@@ -21,7 +21,7 @@ export default function Header() {
                <a href='#' className='bg-pink-600 text-indigo-100 font-bold mt-2 px-4 py-2 rounded-full text-xs uppercase hover:bg-pink-900'>Shop Now</a>
                </div>
                <div className='absolute right-0 bg-white h-24 w-24 flex justify-center items-center rounded-full border-4 border-pink-500'>
-                <img src='/Images/spyder logo.png' className=''></img>
+                <img src='/spyder logo.png' className=''></img>
                </div>
             </div>
 
