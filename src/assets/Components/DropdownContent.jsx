0f5a72db2bbdbe43e092d256c26548bf8ad20b0f
Zubair <zubairzahid228@gmail.com>
2023-08-23 16:21:17 +0500

    ni-commit

--- src/assets/Components/DropdownContent.jsx
+++ src/assets/Components/DropdownContent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DropdownContent = () => {
   return (
-    <div className="inline-flex justify-center items-center bg-white border border-cyan-200 bg-cyan-900 border rounded-xl shadow-lg select-none mr-2">
+    <div className="inline-flex justify-center items-center border border-cyan-200 bg-cyan-900 border rounded-xl shadow-lg select-none mr-2">
     <a
         href="#home-section"
         className="block p-2 text-indigo-100 transition-colors hover:bg-cyan-100 hover:rounded-lg hover:text-pink-600"
